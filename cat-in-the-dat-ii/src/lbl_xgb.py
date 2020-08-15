b00c545ced2a6236371d101efee5a1db7eec4b94
Santiago Viquez <santi.viquez@gmail.com>
2020-08-15 08:44:56 -0600

    Add xgboost

---
+++ cat-in-the-dat-ii/src/lbl_xgb.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import xgboost as xgb
+from sklearn import metrics
+from sklearn import preprocessing
+
+
+def run(fold):
+    # load the full training data with folds
+    df = pd.read_csv('../input/train_folds.csv')
+
+    # all columns are features except id and target columns
+    features = [
+        f for f in df.columns if f not in ('id', 'target', 'kfold')
+    ]
+
+    # fill all NaN values with NONE
+    for col in features:
+        df.loc[:, col] = df[col].fillna("NONE").astype(str)
+
+    # lets label encode all the features
+    for col in features:
+        # initialize LabelEncoder
+        lbl = preprocessing.LabelEncoder()
+        # fit LabelEncoder with all the data
+        lbl.fit(df[col])
+        # transform all the data
+        df.loc[:, col] = lbl.transform(df[col])
+
+    # get training data using folds
+    df_train = df[df['kfold'] != fold].reset_index(drop=True)
+
+    # get validation data using folds
+    df_valid = df[df['kfold'] == fold].reset_index(drop=True)
+
+    # get training data
+    x_train = df_train[features].values
+
+    # get validation data
+    x_valid = df_valid[features].values
+
+    # initialize XGBoost model
+    model = xgb.XGBClassifier(
+        n_jobs=-1,
+        max_depth=7,
+        n_estimators=200
+    )
+
+    # fit model on training data
+    model.fit(x_train, df_train.target.values)
+
+    # predict on validation data
+    valid_preds = model.predict_proba(x_valid)[:, 1]
+
+    # get roc auc score
+    auc = metrics.roc_auc_score(df_valid.target.values, valid_preds)
+
+    # print auc
+    print(f'Fold = {fold}, AUC = {auc}')
+
+
+if __name__ == '__main__':
+    for fold_ in range(5):
+        run(fold_)
